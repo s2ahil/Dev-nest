97d4170fcb44fae68cc17f7e8ed849282f07a110
s2ahil <101473078+s2ahil@users.noreply.github.com>
2024-02-17 00:14:03 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { ContentCard } from "@/components/HomeContent";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
 // home page
 export default function Home() {
   return (
